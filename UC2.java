09fb5552e13506a02dd304fd4ef65929d6bf1bab
Si-SRM2025 <rs9645srmist.edu.in>
2026-03-19 16:50:22 +0530

    UC2 completed

---
+++ UC2.java
@@ -0,0 +1,12 @@
+public class UC2 {
+    public static void main(String[] args) {
+        // Line by line printing to form "OOPS" banner
+        System.out.println(" ****   ****   ****  *****");
+        System.out.println("*    * *    * *    * *    ");
+        System.out.println("*    * *    * *    * *    ");
+        System.out.println("*    * *    * ****   **** ");
+        System.out.println("*    * *    * *       *   ");
+        System.out.println("*    * *    * *       *   ");
+        System.out.println(" ****   ****  *       *****");
+    }
+}
